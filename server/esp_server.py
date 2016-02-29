b568cc0ad63b5774388c17c62600c3c72dfe7863
Nathan Bergey <nathan.bergey@gmail.com>
2016-02-29 10:55:34 -0700

    Server: Index page: Load a template. Basic lint and test

--- server/esp_server.py
+++ server/esp_server.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return render_template('base.html')
+
+
+@app.route("/")
+def test():
+    return render_template('base.html')
+
 
 if __name__ == "__main__":
     app.run()
---
+++ server/esp_server_test.py
@@ -0,0 +1,16 @@
+import unittest
+import esp_server
+
+
+class ServerTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.app = esp_server.app.test_client()
+
+    def test_index_load(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+
+
+if __name__ == '__main__':
+    unittest.main()
