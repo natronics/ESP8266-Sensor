af5f143322c994e2a07404ea096522eb954359b7
Nathan Bergey <nathan.bergey@gmail.com>
2016-03-02 14:45:40 -0700

    Server: Format table of values

--- server/src/js/index.js
+++ server/src/js/index.js
@@ -1,5 +1,14 @@
 require("../style.sass");
 var $ = require('jquery');
+var dateFormat = require('dateformat');
+
+var sensor_def = {
+  input_voltage: 3.3,
+  resolution: 1023,
+  rate: 0.01,
+  conversion: -273.15,
+  bias: -5.4
+}
 
 $(document).ready(function() {
 
@@ -7,7 +16,12 @@ $(document).ready(function() {
         .done(function(data) {
             for (var i in data.data) {
                 var line = data.data[i];
-                $("#datatable").append("<tr><td>" + line.time + "</td><td>" + line.value + "</td></tr>");
+
+                var date = new Date(line.time*1000);
+
+                var raw_temp = line.value;
+                var temp = ((sensor_def.input_voltage / (sensor_def.resolution / raw_temp)) / (sensor_def.rate)) + sensor_def.conversion - sensor_def.bias;
+                $("#datatable").append('<tr><td class="data time">' + dateFormat(date, "yyyy-mm-dd HH:MM:ss Z", true) + '</td><td class="data raw">' + raw_temp + '</td><td class="data temp">' + temp.toFixed(2) + '</td></tr>');
             }
         });
 });
